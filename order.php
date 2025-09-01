beed3ee88cf610c3e16c74d6d0e5007b090f7e18
Ajit Adhikari <131779702+AjitAdhikari@users.noreply.github.com>
2025-09-01 17:15:15 +0545

    Uncomment order processing code in order.php

--- order.php
+++ order.php
@@ -13,25 +13,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Prepare and bind
-//$stmt = $conn->prepare("INSERT INTO orders (name, phone_number, order_name, additional_food, table_number, message) VALUES (?, ?, ?, ?, ?, ?)");
-//$stmt->bind_param("sssiss", $name, $phone_number, $order_name, $additional_food, $table_number, $message);
-// Get form data
-//$name = $_POST['name'];
-//$phone_number = $_POST['phone_number'];
-//$order_name = $_POST['order_name'];
-//$additional_food = $_POST['additional_food'];
-//$table_number = $_POST['table_number'];
-//$message = $_POST['message'];
+Prepare and bind
+$stmt = $conn->prepare("INSERT INTO orders (name, phone_number, order_name, additional_food, table_number, message) VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssiss", $name, $phone_number, $order_name, $additional_food, $table_number, $message);
+Get form data
+$name = $_POST['name'];
+$phone_number = $_POST['phone_number'];
+$order_name = $_POST['order_name'];
+$additional_food = $_POST['additional_food'];
+$table_number = $_POST['table_number'];
+$message = $_POST['message'];
 
-// Execute the statement
-//if ($stmt->execute() === TRUE) {
-    //echo "Order placed successfully!";
-//} else {
-    //echo "Error: " . $stmt->error;
-//}
+Execute the statement
+if ($stmt->execute() === TRUE) {
+    echo "Order placed successfully!";
+} else {
+    echo "Error: " . $stmt->error;
+}
 
-// Close statement and connection
-//$stmt->close();
-//$conn->close();
+Close statement and connection
+$stmt->close();
+$conn->close();
 ?>
